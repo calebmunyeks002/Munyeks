bb339b176e92ede35307b9d636b671d3a6f5158d
Caleb <calebmunyeks002@gmail.com>
2026-06-30 15:59:19 +0300

    First Commit

---
+++ src/main/java/Munyeks/Munyeks_backend/MunyeksBackendApplication.java
@@ -0,0 +1,13 @@
+package Munyeks.Munyeks_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunyeksBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MunyeksBackendApplication.class, args);
+	}
+
+}
